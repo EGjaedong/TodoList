0d2566a56004f524e3675bd1fe122a7de23d4ace
unknown <hzh_innovation@163.com>
2020-08-21 21:12:50 +0800

    [hzh] feat: add entity User

---
+++ app/src/main/java/com/hezhiheng/todolist/db/entity/User.java
@@ -0,0 +1,59 @@
+package com.hezhiheng.todolist.db.entity;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.Objects;
+
+@Entity(tableName = "user")
+public class User {
+    @NonNull
+    @PrimaryKey
+    private int id;
+
+    @ColumnInfo
+    private String name;
+
+    @ColumnInfo
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name) &&
+                Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, password);
+    }
+}
