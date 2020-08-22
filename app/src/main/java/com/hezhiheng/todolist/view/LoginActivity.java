6c0dc60ccd43b192e642b2c8fa8c956b9a2b41bf
unknown <hzh_innovation@163.com>
2020-08-22 21:50:35 +0800

    [hzh] feat: add username and password judge to show error button

--- app/src/main/java/com/hezhiheng/todolist/view/LoginActivity.java
+++ app/src/main/java/com/hezhiheng/todolist/view/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.hezhiheng.todolist.view;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,17 +14,27 @@ import androidx.lifecycle.ViewModelProvider;
 import com.hezhiheng.todolist.R;
 import com.hezhiheng.todolist.viewmodel.UserViewModel;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.OnTextChanged;
 
 public class LoginActivity extends AppCompatActivity {
     private UserViewModel model;
+    private String usernamePattern = "^[\\d|a-z|A-Z]{3,12}$";
+    private String passwordPattern = "^[\\w|\\W]{6,18}$";
 
     @BindView(R.id.edit_username)
-    EditText username;
+    EditText editUsername;
     @BindView(R.id.edit_password)
-    EditText password;
+    EditText editPassword;
+    @BindView(R.id.btn_error_username)
+    ImageButton errorUsernameBtn;
+    @BindView(R.id.btn_error_password)
+    ImageButton errorPasswordBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,8 +45,50 @@ public class LoginActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick(R.id.login_btn)
-    void loginClick() {
-        model.login(username.getText().toString(), password.getText().toString());
+    @OnClick({R.id.login_btn, R.id.btn_error_username, R.id.btn_error_password})
+    void loginClick(View view) {
+        switch (view.getId()) {
+            case R.id.login_btn:
+                model.login(editUsername.getText().toString(), editPassword.getText().toString());
+                break;
+            case R.id.btn_error_username:
+                Toast.makeText(this, "用户名校验错误", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_error_password:
+                Toast.makeText(this, "密码校验错误", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    @OnTextChanged(R.id.edit_username)
+    void judgeUsernameText(CharSequence text) {
+        if (regularJudge(usernamePattern, text.toString())) {
+            hideErrorBtn(errorUsernameBtn);
+        } else {
+            showErrorBtn(errorUsernameBtn);
+        }
+    }
+
+    @OnTextChanged(R.id.edit_password)
+    void judgePasswordText(CharSequence text) {
+        if (regularJudge(passwordPattern, text.toString())) {
+            hideErrorBtn(errorPasswordBtn);
+        } else {
+            showErrorBtn(errorPasswordBtn);
+        }
+    }
+
+    private void hideErrorBtn(ImageButton button) {
+        button.setVisibility(View.INVISIBLE);
+    }
+
+    private void showErrorBtn(ImageButton button) {
+        button.setVisibility(View.VISIBLE);
+    }
+
+    private boolean regularJudge(String patternString, String target) {
+        Pattern pattern = Pattern.compile(patternString);
+        Matcher matcher = pattern.matcher(target);
+        return matcher.matches();
     }
 }
