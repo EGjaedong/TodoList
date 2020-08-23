0d927706af2f29328011d6964c36b2baeb20361d
unknown <hzh_innovation@163.com>
2020-08-23 15:34:46 +0800

    [hzh] feat: add jump to ReminderItemActivity

--- app/src/main/java/com/hezhiheng/todolist/view/MainActivity.java
+++ app/src/main/java/com/hezhiheng/todolist/view/MainActivity.java
@@ -1,18 +1,32 @@
 package com.hezhiheng.todolist.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.hezhiheng.todolist.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.btn_add)
+    ImageButton btnAddRemind;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_add)
+    void btnClick(View view) {
+        Intent intent = new Intent(this, ReminderItemActivity.class);
+        startActivity(intent);
     }
 }
