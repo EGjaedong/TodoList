cb2510ff52e269bdf8d3ab8700fca21a3c944868
unknown <hzh_innovation@163.com>
2020-08-24 18:15:12 +0800

    [hzh] feat: add date display

--- app/src/main/java/com/hezhiheng/todolist/view/MainActivity.java
+++ app/src/main/java/com/hezhiheng/todolist/view/MainActivity.java
@@ -14,7 +14,12 @@ import com.hezhiheng.todolist.R;
 import com.hezhiheng.todolist.db.entity.Reminder;
 import com.hezhiheng.todolist.viewmodel.ReminderItemViewModel;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.TextStyle;
 import java.util.List;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,8 +28,10 @@ import butterknife.OnClick;
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.btn_add)
     ImageButton btnAddRemind;
-    @BindView(R.id.show_remind)
-    TextView showRemindersText;
+    @BindView(R.id.text_date)
+    TextView textDate;
+    @BindView(R.id.text_month)
+    TextView textMonth;
 
     private ReminderItemViewModel remindViewModel;
 
@@ -33,21 +40,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        showDate();
+
         ReminderItemViewModel.Factory factory = new ReminderItemViewModel.Factory();
         remindViewModel = new ViewModelProvider(this, factory).get(ReminderItemViewModel.class);
-        final Observer<List<Reminder>> remindersObserver = new Observer<List<Reminder>>() {
-            @Override
-            public void onChanged(List<Reminder> reminders) {
-                if (reminders.size()!=0){
-                    StringBuilder sb = new StringBuilder();
-                    reminders.forEach(reminder -> {
-                        sb.append(reminder.toString()).append("\n");
-                    });
-                    showRemindersText.setText(sb.toString());
-                }
+        final Observer<List<Reminder>> remindersObserver = reminders -> {
+            if (reminders.size() != 0) {
+                StringBuilder sb = new StringBuilder();
+                reminders.forEach(reminder -> {
+                    sb.append(reminder.toString()).append("\n");
+                });
+                // TODO: 2020/8/24 add remindList show
             }
         };
         remindViewModel.getReminders().observe(this, remindersObserver);
+    }
+
+    private void showDate() {
+        LocalDate localDate = LocalDate.now();
+        Month month = localDate.getMonth();
+        int dayOfMonth = localDate.getDayOfMonth();
+        DayOfWeek dayOfWeek = localDate.getDayOfWeek();
+        String dayText = dayOfWeek.getDisplayName(TextStyle.FULL, Locale.ENGLISH) + ", "
+                + dayOfMonth + "th";
+        textDate.setText(dayText);
+        textMonth.setText(month.getDisplayName(TextStyle.SHORT, Locale.ENGLISH));
     }
 
     @OnClick(R.id.btn_add)
